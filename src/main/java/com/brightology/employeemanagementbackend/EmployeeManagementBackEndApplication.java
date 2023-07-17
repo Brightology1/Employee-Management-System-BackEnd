b998e0ea9b2d620d5f2e43bd695addecb325a162
HP 250g7 <brightology2@gmain.com>
2023-07-17 01:02:28 +0100

    Cross Origin

--- src/main/java/com/brightology/employeemanagementbackend/EmployeeManagementBackEndApplication.java
+++ src/main/java/com/brightology/employeemanagementbackend/EmployeeManagementBackEndApplication.java
@@ -19,23 +19,23 @@ public class EmployeeManagementBackEndApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Employee employee = new Employee();
-        employee.setFirstName("Bright");
-        employee.setLastName("Ugochukwu");
-        employee.setEmailId("brightology@gmail");
-        employeeRepository.save(employee);
-
-        Employee employee1 = new Employee();
-        employee1.setFirstName("Jacinta");
-        employee1.setLastName("Ugochi");
-        employee1.setEmailId("elochukwu@gmail.com");
-        employeeRepository.save(employee1);
-
-        Employee employee2 = new Employee();
-        employee2.setFirstName("Adaugo");
-        employee2.setLastName("Delight");
-        employee2.setEmailId("adaugo@gmail.com");
-        employeeRepository.save(employee2);
+//        Employee employee = new Employee();
+//        employee.setFirstName("Bright");
+//        employee.setLastName("Ugochukwu");
+//        employee.setEmailId("brightology@gmail");
+//        employeeRepository.save(employee);
+//
+//        Employee employee1 = new Employee();
+//        employee1.setFirstName("Jacinta");
+//        employee1.setLastName("Ugochi");
+//        employee1.setEmailId("elochukwu@gmail.com");
+//        employeeRepository.save(employee1);
+//
+//        Employee employee2 = new Employee();
+//        employee2.setFirstName("Adaugo");
+//        employee2.setLastName("Delight");
+//        employee2.setEmailId("adaugo@gmail.com");
+//        employeeRepository.save(employee2);
 
     }
 }
--- src/main/java/com/brightology/employeemanagementbackend/controller/EmployeeController.java
+++ src/main/java/com/brightology/employeemanagementbackend/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package com.brightology.employeemanagementbackend.controller;
 import com.brightology.employeemanagementbackend.model.Employee;
 import com.brightology.employeemanagementbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
@@ -20,5 +22,4 @@ public class EmployeeController {
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
-
 }
